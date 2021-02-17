10414597f88df7f96f532a3b81ce5c04242483eb
Pedro Almeida <59484474+PedroMarianoAlmeida@users.noreply.github.com>
2021-02-17 08:51:32 -0300

    Create randomBetweenHigherExclusive.ts

---
+++ src/functions/randomBetweenHigherExclusive.ts
@@ -0,0 +1,11 @@
+const randomBetweenHigherExclusive = (n1: number, n2: number) => {
+    const higherNumber = n1 > n2 ? n1 : n2;
+    const lowerNumber = n1 > n2 ? n2 : n1;
+    
+    const randomNumber =  Math.random() * (higherNumber - lowerNumber)
+    const randomWithOffset = randomNumber + lowerNumber
+  
+    return Math.floor(randomWithOffset);
+}
+
+export default randomBetweenHigherExclusive;
