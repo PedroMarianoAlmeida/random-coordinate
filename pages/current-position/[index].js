79db28fbafed105cd7f1be93ac471f28e0778f9b
Pedro Almeida <59484474+PedroMarianoAlmeida@users.noreply.github.com>
2021-02-17 13:18:13 -0300

    Fixing bug
    The value in query take some time to load, so I decided to serve it with props

--- pages/current-position/[index].js
+++ pages/current-position/[index].js
@@ -1,9 +1,15 @@
 import {useEffect} from 'react';
 import {useRouter} from 'next/router'
 
-const CurrentPosition = () => {
+export async function getServerSideProps(ctx) {
+    const reach = ctx.query.reach;
+    return {props: {
+        reach
+    }}
+}
+
+const CurrentPosition = ({reach}) => {
     const router = useRouter();
-    const reach = router.query.reach;
 
     useEffect(()=> {
         
@@ -14,7 +20,7 @@ const CurrentPosition = () => {
             const adress = `/api/origin-point/=?latitude=${latitude}&longitude=${longitude}&reach=${reach}`
             router.push(adress);
         })    
-           
+
     }, [])    
     
     return (
