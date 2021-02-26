ae8bc8902397bec9f4045eb0a261c8889436e903
Pedro Almeida <59484474+PedroMarianoAlmeida@users.noreply.github.com>
2021-02-26 14:46:46 -0300

    Revert "Endpoint traking/minute implemented"
    This reverts commit f342960ee5f9402a3b4adffe7934c9648bbaa376.

--- pages/api/tracking/minute.js
+++ pages/api/tracking/minute.js
@@ -1,18 +1,17 @@
-import { initFirebase, db } from '../../../src/config/firebaseConfig';
+import {initFirebase, db} from '../../../src/config/firebaseConfig';
 
 export default (req, res) => {
     initFirebase();
-
-    db.collection('coordenates').doc('every-minute').get().then(
-        (doc) => {
-            const coordenate = doc.data().coordenate;
-
-            res.setHeader('Cache-Control', 's-maxage=60 , stale-while-revalidate');
-
-            res.status(200).json({
-                latitude: coordenate._lat,
-                longitude: coordenate._long
-            })
+    
+    db.collection('coordenates').get().then(
+        (snapshot) => {
+           console.log(snapshot.docs[0].data()); 
         }
     )
-}
+    
+    res.status(200).json({ 
+        name: 'John DoEe'
+    //   latitude, 
+    //   longitude
+    })
+  }
