12447d5e493920475274d841009e1d0c9449f725
Pedro Almeida <59484474+PedroMarianoAlmeida@users.noreply.github.com>
2021-02-26 14:46:20 -0300

    Revert "Refactoring origin-point endpoint"
    This reverts commit 1c5c48fd2a8cea67f1e15d8d28ad7fca94c81e67.

--- pages/api/origin-point/[index].js
+++ pages/api/origin-point/[index].js
@@ -1,11 +1,25 @@
-import newRandomCoordenate from './../../../src/functions/newRandomCoordenate';
+import randomBetween from './../../../src/functions/randomBetween.ts';
+
+
 
 export default (req, res) => {    
     const originLatitude = Number(req.query.latitude);
     const originLongitude = Number(req.query.longitude);
     const reachInKilometters = Number(req.query.reach);
 
-    const [ latitude, longitude ] = newRandomCoordenate(originLatitude, originLongitude, reachInKilometters);
+    const latitudeReach = reachInKilometters / 110.574;
+
+    const originLatitudeInRadius = degreeToRadians(originLatitude);
+    const longitudeReach = reachInKilometters / ( 111.320 * Math.cos(originLatitudeInRadius) );
+
+    const bottomLatitude = originLatitude - latitudeReach < -90 ? -90 : originLatitude - latitudeReach;
+    const topLatitude = originLatitude + latitudeReach > 90 ? 90 : originLatitude + latitudeReach;
+
+    const bottomLongitude = originLongitude - longitudeReach < -180 ? -180 : originLongitude - longitudeReach;
+    const topLongitude = originLongitude + longitudeReach > 180 ? 180 : originLongitude + longitudeReach;
+
+    const latitude = randomBetween( bottomLatitude, topLatitude);
+    const longitude = randomBetween(bottomLongitude, topLongitude);
 
     res.status(200).json({ 
         latitude, 
