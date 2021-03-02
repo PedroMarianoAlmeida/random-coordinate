7178a3028be1b2fe841867a98922120ebc4dfa2d
Pedro Almeida <59484474+PedroMarianoAlmeida@users.noreply.github.com>
2021-03-02 15:56:09 -0300

    Random By local API refactored and documentation implemented

--- pages/api/random/by-local-name/[index].js
+++ pages/api/random/by-local-name/[index].js
@@ -1,18 +1,19 @@
+import calculateNewCoordinate from './../../../../src/functions/latitudeAndLongitudeHandling/calculateNewCoordinate';
+
 export default async(req, res) => {
     const address = req.query.address;
     const googleCloudKey = req.query.googleCloudKey;
-    const reach = req.query.reach;
+    const distanceKm = req.query.km ? Number(req.query.km) : Number(req.query.miles) * 0.621371;
 
     const response = await fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(address)}&key=${googleCloudKey}`);
     const data = await response.json();
     
-    if(data.status === 'OK') {
-        const dataTreated = data.results[0].geometry.location
-        const newUrl = `/api/origin-point/=?latitude=${dataTreated.lat}&longitude=${dataTreated.lng}&reach=${reach}`;
-        res.writeHead(302, {
-            'Location': newUrl
-          });
-        res.end();
+    if(data.status === 'OK') {       
+      const dataTreated = data.results[0].geometry.location
+      const currentAngle = 2 * Math.PI * Math.random(); //A random number between 0 and 2π
+      const currentDistance = distanceKm * Math.random(); //A random number between 0 and the max distance
+      const coordinate = calculateNewCoordinate(dataTreated.lat, dataTreated.lng, currentAngle, currentDistance)
+      res.status(200).json(coordinate)
     }
     else {
         console.log(data);
---
+++ pages/documentation/random-by-local-name.js
@@ -0,0 +1,114 @@
+import { useState, useEffect } from 'react'
+import GoogleMapComponent from '../../src/components/GoogleMap';
+
+import calcualateZoomMap from './../../src/functions/calculateZoomMap.ts';
+
+const TrackingOriginPoint = () => {
+    const [localName, setLocalName] = useState('Halifax');
+    const [googleCloudKey, setGoogleCloudKey] = useState('YOUR_API_KEY');
+
+    const [latitude, setLatitude] = useState(0);
+    const [longitude, setLongitude] = useState(0);
+    const [distanceNumber, setDistanceNumber] = useState(100);
+    const [distanceUnit, setDistanceUnit] = useState('km');
+
+    const [endPoint, setEndPoint] = useState('');
+    const [coordinates, setCoordinates] = useState([]);
+
+    const [zoomMap, setZoomMap] = useState(calcualateZoomMap(distanceNumber, distanceUnit));
+
+    useEffect(() => {
+        setZoomMap(calcualateZoomMap(distanceNumber, distanceUnit))
+    }, [distanceNumber, distanceUnit])
+
+    const getOriginPoint = async () => {
+        try {
+            const response = await fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(localName)}&key=${googleCloudKey}`);
+            if (!response.ok) throw Error(response.statusText);
+            const result = await response.json();
+            const dataTreated = result.results[0].geometry.location
+            console.log(dataTreated);
+            const originLatitude = dataTreated.lat;
+            const originLongitude = dataTreated.lng;
+            setLatitude(originLatitude);
+            setLongitude(originLongitude);
+            console.log(`Origin Point: latitude: ${originLatitude} - Logitude: ${originLongitude}`);
+        }
+        catch (err) {
+            console.log('getOriginPoint', err);
+        }
+    }
+
+    const callAPI = async () => {
+        try {
+            let response = await fetch(`${process.env.ADDRESS}/${endPoint}`);
+            if (!response.ok) throw Error(response.statusText);
+            const result = await response.json();
+            setCoordinates([{ lat: result.latitude, lng: result.longitude }]);
+            console.log(result)
+        }
+        catch (err) {
+            console.log("callAPI", err);
+        }
+    }
+
+    const handleClick = () => {
+        getOriginPoint();
+        callAPI();
+    }
+
+    useEffect(() => {
+        setEndPoint(`api/random/by-local-name/=?address=${localName}&googleCloudKey=${googleCloudKey}&${distanceUnit}=${distanceNumber}`);
+    }, [latitude, longitude, distanceNumber, distanceUnit, localName, googleCloudKey])
+
+    return (
+        <>
+            <form className="text-sm font-semibold">
+                <div className='text-center mb-3 border border-black rounded p-2 mx-4 grid grid-cols-1 md:grid-cols-3'>
+                    <div>
+                        <label className='mx-2'>Address</label>
+                        <input type='text' onChange={(e) => setLocalName(e.target.value)} value={localName} className='mt-1 text-center border-none bg-gray-100 h-11 rounded-xl shadow-lg hover:bg-blue-100 focus:bg-blue-100 focus:ring-0 w-100' />
+                    </div>
+
+                    <div>
+                        <label className='mx-2'>Google Cloud Key</label>
+                        <input type='text' onChange={(e) => setGoogleCloudKey(e.target.value)} value={googleCloudKey} className='mt-1 text-center border-none bg-gray-100 h-11 rounded-xl shadow-lg hover:bg-blue-100 focus:bg-blue-100 focus:ring-0' />
+                    </div>
+
+                    <div>
+                        <label className='mx-2'>Radius</label>
+                        <input type='number' onChange={(e) => setDistanceNumber(e.target.value)} value={distanceNumber} className='mt-1 text-center border-none bg-gray-100 h-11 rounded-xl shadow-lg hover:bg-blue-100 focus:bg-blue-100 focus:ring-0 w-20' />
+                        <select onChange={(e) => setDistanceUnit(e.target.value)} className='mt-1 text-center border-none bg-gray-100 h-11 rounded-xl shadow-lg hover:bg-blue-100 focus:bg-blue-100 focus:ring-0 ml-2'>
+                            <option value='km' selected={distanceUnit == 'km'}>km</option>
+                            <option value='miles' selected={distanceUnit == 'miles'}>miles</option>
+                        </select>
+
+                    </div>
+
+                </div>
+
+
+            </form>
+            <div className='my-2 text-center'><p className='inline'>Endpoint:</p> <code className='bg-black p-2 text-white break-all text-left'>{`https://dummy-coordinate.vercel.app/${endPoint}`}</code></div>
+            <div className='my-2 text-center'><button className='border-2 border-blue-500 rounded-full font-bold text-blue-500 px-4 py-3 transition duration-300 ease-in-out hover:bg-blue-500 hover:text-white mr-6' onClick={handleClick}>Call API</button> <span className={coordinates.length === 0 ? 'hidden' : ''}>Result: {JSON.stringify(coordinates)}</span></div>
+            <div className='flex items-center justify-center'>
+                <div>
+                    <GoogleMapComponent
+                        defaultLat={latitude}
+                        defaultLng={longitude}
+                        defaultZoom={zoomMap}
+                        iconMarker1="marker"
+                        iconMarker2="origin"
+                        mapStyle="lostInDesert"
+                        markers1={coordinates}
+                        markers2={[{ lat: latitude, lng: longitude }]}
+                        sizeIconMarker1={25}
+                        sizeIconMarker2={25}
+                    />
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default TrackingOriginPoint;
